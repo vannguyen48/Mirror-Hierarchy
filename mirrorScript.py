276d1240c003dbda32631b3bc90a13de4e3d3680
Van Nguyen <vanessanguyen382@gmail.com>
2018-03-10 19:35:15 -0800

    Added comments to source code

--- mirrorScript.py
+++ mirrorScript.py
@@ -2,9 +2,24 @@ import argparse
 import os
 
 def main():
+    """
+    Main function.
+
+    """
     parse()
 
 def parse():
+    """
+    Function to process input and parse command-line arguments.
+
+    Args:
+        BASE (str): the base directory containing the file hierarchy.
+        OUTPUT (str): the output directory containing the location of the output file hierarchy.
+
+    Returns:
+        returns 0 if no error found.
+
+    """
     parser = argparse.ArgumentParser(description = 'Create a mirror directory hierarchy.')
     parser.add_argument('BASE', type = str,
                   help = 'the path to the directory where files or SQL scripts are located.')
@@ -14,8 +29,17 @@ def parse():
     assert os.path.isdir(args.BASE), 'Base path is invalid'
     assert os.path.isdir(args.OUTPUT), 'Output path is incorrect'
     extract(args.BASE, args.OUTPUT)
+    return 0
 
 def extract(basepath, outputpath):
+    """
+    Function to extract the base path and record the file hierarchy.
+
+    Args:
+        basepath: the base directory containing the file hierarchy.
+        outputpath: output hierarchy containing the location of the output hierarchy.
+
+    """
     path_list = []
     print(path_list)
     for root, dirs, files in os.walk(basepath):
@@ -31,6 +55,14 @@ def extract(basepath, outputpath):
     join(path_list, outputpath)
 
 def join(pathlist, outputpath):
+    """
+    Composes output hierarchy.
+
+    Args:
+        pathlist: list containing directories that needs to be created in output directory.
+        outputpath: directory containing the location of ouput directory hierarchy.
+
+    """
     output_path_list = []
     print (outputpath)
     for i in pathlist:
@@ -39,10 +71,21 @@ def join(pathlist, outputpath):
     create_hierarchy(output_path_list)
 
 def create_hierarchy(output_path_list):
+    """
+    Creates directory hierarchy in output location.
+
+    Args:
+        output_path_list: list containing all mirrored file hierarchy.
+
+    """
     for i in output_path_list:
         if not os.path.exists(i):
             print (i)
             os.makedirs(i)
 
 if __name__ == '__main__':
+    """
+    Performs a main check so that this script can run on its own (rather than being imported from another module).
+
+    """
     main()
